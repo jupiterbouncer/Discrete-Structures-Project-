babc46c7345da73480039942e97f32c0167aa077
jupiterbouncer <demisubair11@gmail.com>
2025-11-12 23:07:31 +0000

    1. Created and completed the User class as a separate file. 2. Deleted the user class from the TutorApp.java file to prevent duplicate errors 3. Created the ScoreTracker.java class

---
+++ TutorApp.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+
+public class TutorApp {
+
+    // feedback and scoring system
+}
+
+
+class Definitions{
+
+    // Basic details
+    private String title;
+    private String description;
+    private ArrayList<String> content;
+
+    // Activities and assessment
+    private ArrayList<Exercise> exercises;
+    private int totalPoints;
+    private int score;
+
+    // Feedback/tracking
+    private boolean completed;
+    private int hintCount;
+
+    public Definitions(String title, String description){
+        this.title = title;
+        this.description = description;
+        this.content = new ArrayList<>();
+        this.exercises = new ArrayList<>();
+        this.totalPoints = 0;
+        this.score = 0;
+        this.completed = false;
+        this.hintCount = 0;
+    }
+
+    public void displayContent(){
+
+    }
+
+    public void startExercises(){
+
+    }
+
+    public double getCompletionRate(){
+
+    }
+
+    public void addExercise(Exercise exercise){
+
+    }
+}
+
+class Exercise{
+
+}
+
+class LogicalConnectives{
+    // logical connectives
+}
+
+class TruthTables{
+    // truth tables
+
+}
+
+class ExpressingConditionals{
+    // the different ways of expressing conditionals
+    
+}
+
+class CIC{
+    // the converse, inverse, and contrapositive of a conditional
+}
+
+class FeedbackEngine{
+
+}
+
+class HintSystem{
+
+}
+
+class ScoreTracker{
+
+}
---
+++ User.java
@@ -0,0 +1,93 @@
+import java.util.HashSet; //
+public class User {
+    /* This class represents a learner using our programme
+    Contains learner's identity and progress
+     */
+    // Attributes
+    private String username;                      // Stores user's name
+    private int currentLevel;                     // Stores user's current difficulty level
+    private String currentTopicSection;           // Indicates the section the user has reached
+    private ScoreTracker scoreTracker;            // Stores user's score
+    private int availableHints;                   // stores number of hints left
+    private HashSet<String> completedModules;     // Stores completed modules in a HashSet
+
+    // Constructor
+    public User(String username){
+        this.username = username;
+        this.currentLevel = 1;                            // Makes every user begin from difficulty level 1
+        this.currentTopicSection = "Definitions";         // Definitions should be where each user starts from
+        this.availableHints = 3;                          // Users begin with 3 hints
+        this.scoreTracker = new ScoreTracker();
+        this.completedModules = new HashSet<>();
+    }
+
+    //Getters and setters
+    public String getUserName() {
+        return username;
+    }
+
+    public void setUserName(String userName) {
+        this.username = userName;
+    }
+
+    public int getCurrentLevel() {
+        return currentLevel;
+    }
+
+    public void setCurrentLevel(int currentLevel) {
+        this.currentLevel = currentLevel;
+    }
+
+    public String getCurrentTopicSection() {
+        return currentTopicSection;
+    }
+
+    public void setCurrentTopicSection(String currentTopicSection) {
+        this.currentTopicSection = currentTopicSection;
+    }
+
+    public ScoreTracker getScoreTracker() {
+        return scoreTracker;
+    }
+
+    public int getAvailableHints() {
+        return availableHints;
+    }
+
+    // Methods
+    // Checks if a user can use any more hints
+    public boolean canUseHint(){
+        return availableHints > 0;
+    }
+
+    // Method for decrementing hints
+    public void useHint(){
+        if (canUseHint()) availableHints--;
+    }
+
+    // Method to give user's hints possibly as a reward
+    public void addHint(int n){
+        if (n > 0) availableHints += n;
+    }
+
+    // Method to add a module to the hashset of completed modules
+    public void updateCompletedModules(String moduleName){
+        completedModules.add(moduleName);
+    }
+
+    // Checks if a user has completed a particular module
+    public boolean hasCompletedModule(String moduleName){
+        return completedModules.contains(moduleName);
+    }
+
+    // Checks the modules that have been completed
+    public HashSet<String> getCompletedModules(){
+        return completedModules;
+    }
+
+
+
+
+
+}
+
